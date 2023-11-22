db6d0b8e7e864c8b4d857540697f6919abaadd37
Melo225 <melombaoh@gmail.com>
2023-11-22 09:06:55 +0000

    new updates

--- LearnDapper/AppDbContext.cs
+++ LearnDapper/AppDbContext.cs
@@ -1,6 +1,21 @@
+using LearnDapper.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace LearnDapper;
 
-public class AppDbContext
+public class AppDbContext : DbContext
 {
-    
+    private readonly IConfiguration _configuration;
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<Todo> Todos { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+    }
+
 }
--- LearnDapper/Program.cs
+++ LearnDapper/Program.cs
@@ -1,10 +1,12 @@
 using LearnDapper;
-using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
